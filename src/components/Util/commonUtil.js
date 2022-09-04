5f8f144cf7e6ec20e553dd29ae0125b0f94b75ba
shkim <thiagoo@naver.com>
2022-09-04 10:56:15 +0900

    fixed the function to make random number - 선수 생성 시 랜덤하게 스탯을 뽑아내기 위한 함수 수정 - 주석 추가

--- src/components/Util/commonUtil.js
+++ src/components/Util/commonUtil.js
@@ -1,13 +1,15 @@
 const Util = {
   randomOfArray: (array, max) => {
+    //배열에서 하나의 요소를 랜덤으로 리턴
     const maxRandomVal = Math.floor(Math.random() * array.length)
     return array[maxRandomVal];
   },
 
    makeRandomNumber: (max=null, min=null) => {
-    //min 값은 1부터 가능함
+    //min 값은 1부터 넣어야함
+    //최대값과 범위 안의 값을 반환할 수 있으며, 최소값 기능은 없음
       if(max && !min){
-        const maxRandomVal = Math.random() * posList.length
+        const maxRandomVal = Math.random() * max
         return Math.floor(maxRandomVal)
       }else if(max && min){
         const bothRandomVal = (Math.random()*(max-min))+min
