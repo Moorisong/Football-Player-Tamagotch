dabf422453a1f622db0cb7192ffcd4a830e7fddb
shkim <thiagoo@naver.com>
2023-02-01 21:21:56 +0900

    fixed Prevent from adding injury data to the training model - training 모델에 부상과 관련한 데이터가 추가되는 부분 주석 처리함

--- src/Util/commonUtil.js
+++ src/Util/commonUtil.js
@@ -127,8 +127,8 @@ const Util = {
 
       // 10퍼센트의 확률로 부상 발생
       if (injuryLuckyNum <= 10) {
-        pModel.injury.onInjury = true
-        pModel.injury.startTime = new Date()
+        // pModel.injury.onInjury = true
+        // pModel.injury.startTime = new Date()
 
         tModel.injury += 1
         await tModel.save()
