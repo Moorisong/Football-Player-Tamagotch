3f5f70497b67184592ac2d4e66e97913bee9a6f6
shkim <thiagoo@naver.com>
2023-04-08 16:53:52 +0900

    fixed Refactor register page - 로그인 및 가입 페이지 수정

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -5,6 +5,8 @@ import * as util from '../../Util/Api'
 export default function Register() {
   const [idAdded, setIdAdded] = useState(false)
 
+  let id, pw, age, sex, nickName, club
+
   const input_id = useRef(null)
   const input_pw = useRef(null)
   const input_age = useRef(null)
@@ -12,107 +14,143 @@ export default function Register() {
   const input_nickName = useRef(null)
   const input_club = useRef(null)
 
-  function submitRegister() {
-    const id = input_id.current.value
-    const pw = input_pw.current.value
-    const age = input_age.current.value
-    const sex = input_sex.current.value
-    const nickName = input_nickName.current.value
-    const club = input_club.current.value
+
+
+  const inputInfo = [
+    {
+      name: 'id',
+      placeholder: 'ID 입력',
+      ref: input_id,
+    },
+    {
+      name: 'pw',
+      placeholder: 'PW 입력',
+      ref: input_pw,
+    },
+    {
+      name: 'age',
+      placeholder: '나이 입력',
+      ref: input_age,
+    },
+    {
+      name: 'sex',
+      placeholder: '성별 입력',
+      ref: input_sex,
+    },
+    {
+      name: 'nickName',
+      placeholder: '닉네임 입력',
+      ref: input_nickName,
+    },
+    {
+      name: 'club',
+      placeholder: '클럽 입력',
+      ref: input_club
+    },
+  ]
+
+  function valueCheck() {
 
     let check = /^[0-9]+$/
+    // const eleArr = ['id', 'pw', 'age', 'sex', 'nickName', 'club']
 
-    if (!id || !pw || !age || !sex || !nickName || !club) {
-      alert('모든 항목을 입력해주세요.')
-      return false
+    inputInfo.forEach((ele)=>{
+      // 에러난다 송현아
+      const val = ele.ref.current.value
+      if(!val){
+        alert('모든 항목을 입력해주세요.')
+        return false
+      }
+      if(ele.name === 'id' && val.length > 20){
+        alert('ID는 20글자까지 허용됩니다. 현재 글자수 : ' + id.length)
+        return false
+      }
+      if(ele.name === 'pw' && val.length < 3){
+        alert('패스워드는 3글자 이상으로 설정해주세요.')
+        return false
+      }
+      if(ele.name === 'age' && !check.test(val)){
+        alert('나이는 숫자로 입력해주세요.')
+        return false
+      }
+      if(ele.name === 'sex' && !check.test(val)){
+        alert('성별은 문자로 입력해주세요.')
+        return false
+      }
+
+    })
+
+    // id = input_id.current.value
+    // pw = input_pw.current.value
+    // age = input_age.current.value
+    // sex = input_sex.current.value
+    // nickName = input_nickName.current.value
+    // club = input_club.current.value
+
+    // if (!id || !pw || !age || !sex || !nickName || !club) {
+    //   alert('모든 항목을 입력해주세요.')
+    //   return false
+    // }
+
+    // if (id.length > 20) {
+    //   alert('ID는 20글자까지 허용됩니다. 현재 글자수 : ' + id.length)
+    //   return false
+    // } else if (pw.length < 3) {
+    //   alert('패스워드는 3글자 이상으로 설정해주세요.')
+    //   return false
+    // } else if (!check.test(age)) {
+    //   alert('나이는 숫자로 입력해주세요.')
+    //   return false
+    // } else if (check.test(sex)) {
+    //   alert('성별은 문자로 입력해주세요.')
+    //   return false
+    // }
+  }
+
+  function submitRegister() {
+
+
+
+    valueCheck()
+
+    const param = {
+      id: id,
+      pw: pw,
+      age: age,
+      sex: sex,
+      nickName: nickName,
+      club: club,
     }
 
-    if (id.length > 20) {
-      alert('ID는 20글자까지 허용됩니다. 현재 글자수 : ' + id.length)
-      return false
-    } else if (pw.length < 3) {
-      alert('패스워드는 3글자 이상으로 설정해주세요.')
-      return false
-    } else if (!check.test(age)) {
-      alert('나이는 숫자로 입력해주세요.')
-      return false
-    } else if (check.test(sex)) {
-      alert('성별은 문자로 입력해주세요.')
-      return false
-    }
-
-      const param = {
-          id: id,
-          pw: pw,
-          age: age,
-          sex: sex,
-          nickName: nickName,
-          club: club,
-        }
-
-
-      const result = util.doReqPost('http://localhost:3001/register', param).then((result) =>{
-        console.log('re---> ', result)
-        if(result.resultMsg === 'success') {
-          // setIdAdded(true)
-          // 가입 성공은 잘 뜨는데 아이디 중복부터 아예 값이 안넘어옴
-          alert('회원 가입 완료! 로그인 해주시기 바랍니다.');
-        }else if(result.resultMsg === 'duplicated ID'){
-          alert("이미 존재하는 ID입니다.");
-          return false
-        }else{
-          alert('에러가 발생하였습니다');
-          console.log('에러---->', result.errorMsg)
-        }
-      })
+    util.doReqPost('http://localhost:3001/register', param).then(result => {
+      if (result.resultMsg === 'success') {
+        setIdAdded(true)
+        alert('회원 가입 완료! 로그인 해주시기 바랍니다.')
+      } else if (result.resultMsg === 'duplicated ID') {
+        alert('이미 존재하는 ID입니다.')
+        return false
+      } else {
+        alert('에러가 발생하였습니다')
+        console.log('Error---->', result.errorMsg)
+      }
+    })
   }
 
   return (
     <>
-      {!idAdded && (
-        <div className={styles.textBoxWrap}>
-          <input
-            type="text"
-            placeholder="ID 입력"
-            ref={input_id}
-            className={styles.textBox}
-          />
-          <input
-            type="text"
-            placeholder="PW 입력"
-            ref={input_pw}
-            className={styles.textBox}
-          />
-          <input
-            type="text"
-            placeholder="나이 입력"
-            ref={input_age}
-            className={styles.textBox}
-          />
-          <input
-            type="text"
-            placeholder="성별 입력"
-            ref={input_sex}
-            className={styles.textBox}
-          />
-          <input
-            type="text"
-            placeholder="닉네임 입력"
-            ref={input_nickName}
-            className={styles.textBox}
-          />
-          <input
-            type="text"
-            placeholder="클럽 입력"
-            ref={input_club}
-            className={styles.textBox}
-          />
-
-          <button onClick={submitRegister} className={styles.RegisterBtn}>
-            가입하기
-          </button>
-        </div>
-      )}
+      <div className={styles.textBoxWrap}>
+        {!idAdded && inputInfo?.map((ele, idx) =>
+            <input key = {ele.ref+idx}
+              type="text"
+              placeholder={ele.placeholder}
+              ref={ele.ref}
+              className={styles.textBox}
+            />
+        )}
+        <button onClick={submitRegister} className={styles.RegisterBtn}>
+          가입하기
+        </button>
+      </div>
     </>
   )
 }
