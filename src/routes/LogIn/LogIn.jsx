70f935040e3651ca4fef015790e42d03bcfacce2
Moorisong <thiagoo@naver.com>
2023-05-26 00:07:13 +0900

    fixed:  Input msg logic
    - Valid 체크 이후 메시지를 표시하는 방식 변경함

--- src/routes/LogIn/LogIn.jsx
+++ src/routes/LogIn/LogIn.jsx
@@ -14,23 +14,25 @@ export default function LogIn() {
   const [pw, setPw] = useState('')
 
   const invailidMsg = useMemo(() => {
-    if (!id && !pw) return 'none'
-    if (id && !pw) return '비밀번호를 입력해주세요.'
-    if (!id && pw) return 'ID를 입력해주세요.'
-    if (id.length > 20) return 'ID는 20자 이하로 생성 가능합니다.'
-    if (pw.length < 3) return '비밀번호는 3자 이상으로 설정해주세요.'
-    return ''
+    let obj = {
+      id: '',
+      pw: ''
+    }
+    if (!id && !pw) { obj.id = ''; obj.pw = '' }
+    if (id && !pw) obj.pw = '비밀번호를 입력해주세요.'
+    if (!id && pw) obj.id = 'ID를 입력해주세요.'
+    if (id.length > 20) obj.id = 'ID는 20자 이하로 입력해주세요.'
+    if (pw && pw.length < 3) obj.pw = '비밀번호는 3자 이상으로 입력해주세요.'
+
+    if (!obj.id && !obj.pw) return false
+    return obj
   }, [id, pw])
 
   function submitLogIn() {
 
-    const param = {
-      id: id,
-      pw: pw,
-    }
+    const param = { id, pw }
 
     doReqPost('http://localhost:3001/logIn', param).then(result => {
-
       if (result.resultMsg === 'notFoundID' || result.resultMsg === 'notFoundPw') return alert('ID나 비밀번호가 올바르지 않습니다.')
       if (result.resultMsg === 'logIn_success') return navigate('/main')
       if (result.errorCode === 'internal error') {
@@ -44,20 +46,20 @@ export default function LogIn() {
     <div className={styles.bobyContainer}>
       <div className={styles.card}>
         <div className={styles.cardContainer}>
-          <p>Log in</p>
+          <p>Sign in</p>
 
           <div className={styles.inputWrap}>
-            <p> ID</p>
+            <p>ID</p>
             <FaceOutlinedIcon className={styles.inputIcon} />
-            <input type="text" onChange={e => setId(e.target.value)} className={cx(styles.input, {[styles.inputBorderActive]: id}) } />
-            { invailidMsg.indexOf('ID') > -1 && <p className={styles.inputTextUnder}> {invailidMsg} </p> }
+            <input type="text" onChange={e => setId(e.target.value)} className={cx(styles.input, { [styles.inputBorderActive]: id })} />
+            {invailidMsg.id && <p className={styles.inputTextUnder}> {invailidMsg.id} </p>}
 
           </div>
           <div className={styles.inputWrap}>
-            <p> PW</p>
+            <p>PW</p>
             <LockOutlinedIcon className={styles.inputIcon} />
-            <input type="password" onChange={e => setPw(e.target.value)} className={cx(styles.input, {[styles.inputBorderActive]: pw}) }  />
-            { invailidMsg.indexOf('비밀번호') > -1 && <p className={styles.inputTextUnder}> {invailidMsg} </p> }
+            <input type="password" onChange={e => setPw(e.target.value)} className={cx(styles.input, { [styles.inputBorderActive]: pw })} />
+            {invailidMsg.pw && <p className={styles.inputTextUnder}> {invailidMsg.pw} </p>}
 
           </div>
 
@@ -65,7 +67,7 @@ export default function LogIn() {
             className={cx(styles.applyBtn, { [styles.applyBtnInvalid]: invailidMsg })}
             type="button"
             onClick={submitLogIn}>
-            Log In
+            Sign in
           </button>
         </div>
       </div>
